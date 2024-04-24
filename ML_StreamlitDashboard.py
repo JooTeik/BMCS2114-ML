b3d7a03c3aa8309ee36c7254a8f732bc2e1d4636
JooTeik <110839309+JooTeik@users.noreply.github.com>
2024-04-24 23:11:21 +0800

    Update ML_StreamlitDashboard.py

--- ML_StreamlitDashboard.py
+++ ML_StreamlitDashboard.py
@@ -1,3 +1,22 @@
 import streamlit as st
 
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+from termcolor import colored
+import pycountry
+import plotly.express as px
+from termcolor import colored
+import squarify
+import folium
+from folium import plugins
+import plotly.graph_objects as go
+from scipy import stats
+
 st.title("Hello World")
+
+
+df = pd.read_csv("earthquake_1995-2023.csv")
+df.head(5)
+
